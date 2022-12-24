08419e7d045fc70aa5eebde78d5299ad1e2d5b16
Thomas Philip <thomas.philip@ecam-strasbourg.eu>
2022-12-24 10:51:27 +0100

    Create Calibration Function

--- calibration.py
+++ calibration.py
@@ -120,129 +120,136 @@ def correctData(corners):
     
     return correctedData
 
+def calibration(imageToCalibrate):
 
-image2=np.array(Image.open('empty.jpeg').resize((400,400)))
-testImage=np.array(Image.open('empty.jpeg').resize((400,400)))
-grayImage = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
-grayImage2 = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
+    #load the image and a test one
+    image2=np.array(Image.open(imageToCalibrate).resize((400,400)))
+    image2=cv2.cvtColor(image2,cv2.COLOR_BGR2RGB)
+    testImage=np.array(Image.open(imageToCalibrate).resize((400,400)))
+    testImage=cv2.cvtColor(testImage,cv2.COLOR_BGR2RGB)
+    grayImage = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
+    grayImage2 = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
 
-retval, corners = cv2.findChessboardCorners(grayImage, (nline, ncol), None)
-correctedData=correctData(corners)
-if (retval):
-    print("there is a board")
-else:
-    print("no board")
+    #look for a 7by7 chessboard pattern
+    retval, corners = cv2.findChessboardCorners(grayImage, (nline, ncol), None)
+    correctedData=correctData(corners)
+    if (retval):
+        print("there is a board")
+    else:
+        print("no board")
 
-fnl = cv2.drawChessboardCorners(grayImage2, (nline, ncol), corners, retval)
-fnlcorrected = cv2.drawChessboardCorners(grayImage, (nline, ncol), correctedData, retval)
-
-print(corners)
-print("now corrected data")
-print(correctedData)
-cv2.imshow('boardcv2',fnl)
-cv2.imshow('boardcorrected',fnlcorrected)
-corners=correctedData
-
-for i in range(49):
-    x=int(corners[i][0][0])
-    y=int(corners[i][0][1])
-    cv2.circle(image2,(x,y),2,(0,0,255),3)
-
-print(corners[0][0][0])
-print(correctedData[0][0][0])
-cv2.rectangle(image2,(int(corners[0][0][0]),int(corners[0][0][1])),(int(corners[8][0][0]),int(corners[8][0][1])),(0,255,0),2)
-
-cv2.namedWindow('board')
-cv2.setMouseCallback('board',mouseClick)
-counter=0
-flag=0
-print("Left Click to define the Y parameter for the upper right corner")
-print("Right Click to define the Y parameter for the lower left corner")
-
-while flag!=2:
-    cv2.imshow('board', image2)
-    if evt==1 and flag==0:
-        yUpperRight = yMouse
-        flag=1
-    if evt==2 and flag==1:
-        yLowerLeft = yMouse
-        flag=2
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-Diag1=Line(image2, int(corners[0][0][1]), int(corners[0][0][0]), int(corners[48][0][1]), int(corners[48][0][0]), yUpperRight, yLowerLeft, "X" )
-flag=0
-print("Left Click to define the Y parameter for the upper left corner")
-print("Right Click to define the Y parameter for the lower right corner")
-
-while flag!=2:
-    cv2.imshow('board', image2)
-    if evt==1 and flag==0:
-        yUpperLeft = yMouse
-        flag=1
-    if evt==2 and flag==1:
-        yLowerRight = yMouse
-        flag=2
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-Diag2=Line(image2, int(corners[6][0][1]), int(corners[6][0][0]), int(corners[42][0][1]), int(corners[42][0][0]), yUpperLeft, yLowerRight, "X" )
-#creation of the bounding rectangle
-cv2.line(image2,(Line.getX(Diag2,yUpperLeft)),(Line.getX(Diag1, yLowerLeft)),(0,0,255),2)
-cv2.line(image2,(Line.getX(Diag2,yLowerRight)),(Line.getX(Diag1, yUpperRight)),(0,0,255),2)
-cv2.line(image2,(Line.getX(Diag2,yUpperLeft)),(Line.getX(Diag1, yUpperRight)),(0,0,255),2)
-cv2.line(image2,(Line.getX(Diag1,yLowerLeft)),(Line.getX(Diag2, yLowerRight)),(0,0,255),2)
-#creating instances for the bounding rectangle
-xUpperLeft,ignore = Line.getX(Diag2,yUpperLeft)
-xLowerLeft,ignore = Line.getX(Diag1, yLowerLeft)
-xUpperRight,ignore = Line.getX(Diag1,yUpperRight)
-xLowerRight,ignore = Line.getX(Diag2,yLowerRight)
-
-lastHorizontal=Line(image2,int(yUpperLeft),int(xUpperLeft),int(yUpperRight),int(xUpperRight),10,490,"Y")
-horizontal1=Line(image2,int(yLowerLeft),int(xLowerLeft),int(yLowerRight),int(xLowerRight),10,490,"Y")
-vertical1=Line(image2,int(yUpperLeft),int(xUpperLeft),int(yLowerLeft),int(xLowerLeft),10,490,"X")
-lastVertical=Line(image2,int(yUpperRight),int(xUpperRight),int(yLowerRight),int(xLowerRight),10,490,"X")
-
-#creating instances for the lines
-#horizontals
-horizontalsArray=[]
-horizontalsArray.append(horizontal1)
-for j in range(0,7):
-    horizontalsArray.append(Line(image2, int(corners[j][0][1]),int(corners[j][0][0]),int(corners[j+42][0][1]),int(corners[j+42][0][0]),10,490,"Y"))
-horizontalsArray.append(lastHorizontal)
-for horizontal in horizontalsArray:
-   Line.drawLines(horizontal)
-#verticals
-indexArray=[0,7,14,21,28,35,42]
-verticalsArray=[]
-verticalsArray.append(vertical1)
-for index in indexArray:
-    verticalsArray.append(Line(image2, int(corners[index][0][1]),int(corners[index][0][0]),int(corners[index+6][0][1]),int(corners[index+6][0][0]),10,490,"X"))
-verticalsArray.append(lastVertical)
-for vertical in verticalsArray:
-    Line.drawLines(vertical)
-
-#we'll try to fill the cell array now
-for i in range(8):
-    for j in range(8):
-        interDuet1=findIntersections(horizontalsArray[j], verticalsArray[i],  1, testImage)
-        interDuet2=findIntersections(horizontalsArray[j+1], verticalsArray[i+1],  1, testImage)
-        duetArray=[interDuet1,interDuet2]
-        coordinates.append(duetArray)
-#print(coordinates)
-#print(len(coordinates))
-
-for i in range(9):
-    for j in range(9):
-        findIntersections(horizontalsArray[i], verticalsArray[j], 1, image2)
-"""
-for i in range(9):
-    for j in range(9):
-        findIntersections(verticalsArray[j], horizontalsArray[i], 1, testImage)
-"""
-findCell(testImage, "C3")
-while True:
-    cv2.imshow('board', image2)
-    cv2.imshow('coordinates',testImage)
+    #displays the visual confirmation for the correction of data
+    fnl = cv2.drawChessboardCorners(grayImage2, (nline, ncol), corners, retval)
+    fnlcorrected = cv2.drawChessboardCorners(grayImage, (nline, ncol), correctedData, retval)
+    print(corners)
+    print("now corrected data")
+    print(correctedData)
     cv2.imshow('boardcv2',fnl)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-    
+    cv2.imshow('boardcorrected',fnlcorrected)
+    corners=correctedData
+
+    #displays a circle for each point
+    for i in range(49):
+        x=int(corners[i][0][0])
+        y=int(corners[i][0][1])
+        cv2.circle(image2,(x,y),2,(0,0,255),3)
+
+    #mouseClick Calibration
+    cv2.namedWindow('board')
+    cv2.setMouseCallback('board',mouseClick)
+    counter=0
+    flag=0
+    print("Left Click to define the Y parameter for the upper right corner")
+    print("Right Click to define the Y parameter for the lower left corner")
+
+    while flag!=2:
+        cv2.imshow('board',image2)
+        if evt==1 and flag==0:
+            yUpperRight = yMouse
+            flag=1
+        if evt==2 and flag==1:
+            yLowerLeft = yMouse
+            flag=2
+        if cv2.waitKey(1) & 0xff ==ord('q'):
+            break  
+
+    Diag1=Line(image2, int(corners[0][0][1]), int(corners[0][0][0]), int(corners[48][0][1]), int(corners[48][0][0]), yUpperRight, yLowerLeft, "X" )
+    flag=0
+    print("Left Click to define the Y parameter for the upper left corner")
+    print("Right Click to define the Y parameter for the lower right corner")
+
+    while flag!=2:
+        cv2.imshow('board', image2)
+        if evt==1 and flag==0:
+            yUpperLeft = yMouse
+            flag=1
+        if evt==2 and flag==1:
+            yLowerRight = yMouse
+            flag=2
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+
+    Diag2=Line(image2, int(corners[6][0][1]), int(corners[6][0][0]), int(corners[42][0][1]), int(corners[42][0][0]), yUpperLeft, yLowerRight, "X" )
+    #creation of the bounding rectangle
+    cv2.line(image2,(Line.getX(Diag2,yUpperLeft)),(Line.getX(Diag1, yLowerLeft)),(0,0,255),2)
+    cv2.line(image2,(Line.getX(Diag2,yLowerRight)),(Line.getX(Diag1, yUpperRight)),(0,0,255),2)
+    cv2.line(image2,(Line.getX(Diag2,yUpperLeft)),(Line.getX(Diag1, yUpperRight)),(0,0,255),2)
+    cv2.line(image2,(Line.getX(Diag1,yLowerLeft)),(Line.getX(Diag2, yLowerRight)),(0,0,255),2)
+    #creating instances for the bounding rectangle
+    xUpperLeft,ignore = Line.getX(Diag2,yUpperLeft)
+    xLowerLeft,ignore = Line.getX(Diag1, yLowerLeft)
+    xUpperRight,ignore = Line.getX(Diag1,yUpperRight)
+    xLowerRight,ignore = Line.getX(Diag2,yLowerRight)
+
+    lastHorizontal=Line(image2,int(yUpperLeft),int(xUpperLeft),int(yUpperRight),int(xUpperRight),10,490,"Y")
+    horizontal1=Line(image2,int(yLowerLeft),int(xLowerLeft),int(yLowerRight),int(xLowerRight),10,490,"Y")
+    vertical1=Line(image2,int(yUpperLeft),int(xUpperLeft),int(yLowerLeft),int(xLowerLeft),10,490,"X")
+    lastVertical=Line(image2,int(yUpperRight),int(xUpperRight),int(yLowerRight),int(xLowerRight),10,490,"X")
+
+    #creating instances for the lines
+    #horizontals
+    horizontalsArray=[]
+    horizontalsArray.append(horizontal1)
+    for j in range(0,7):
+        horizontalsArray.append(Line(image2, int(corners[j][0][1]),int(corners[j][0][0]),int(corners[j+42][0][1]),int(corners[j+42][0][0]),10,490,"Y"))
+    horizontalsArray.append(lastHorizontal)
+    for horizontal in horizontalsArray:
+        Line.drawLines(horizontal)
+    #verticals
+    indexArray=[0,7,14,21,28,35,42]
+    verticalsArray=[]
+    verticalsArray.append(vertical1)
+    for index in indexArray:
+        verticalsArray.append(Line(image2, int(corners[index][0][1]),int(corners[index][0][0]),int(corners[index+6][0][1]),int(corners[index+6][0][0]),10,490,"X"))
+    verticalsArray.append(lastVertical)
+    for vertical in verticalsArray:
+        Line.drawLines(vertical)
+
+    #we'll try to fill the cell array now
+    for i in range(8):
+        for j in range(8):
+            interDuet1=findIntersections(horizontalsArray[j], verticalsArray[i],  1, testImage)
+            interDuet2=findIntersections(horizontalsArray[j+1], verticalsArray[i+1],  1, testImage)
+            duetArray=[interDuet1,interDuet2]
+            coordinates.append(duetArray)
+
+    for i in range(9):
+        for j in range(9):
+            findIntersections(horizontalsArray[i], verticalsArray[j], 1, image2)
+    """
+    for i in range(9):
+        for j in range(9):
+            findIntersections(verticalsArray[j], horizontalsArray[i], 1, testImage)
+    """
+    findCell(testImage, "C3")
+    while True:
+        cv2.imshow('board', image2)
+        cv2.imshow('coordinates',testImage)
+        cv2.imshow('boardcv2',fnl)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+
+    return coordinates,cells
+
+data,labels=calibration('empty.jpeg')
+print(data)
