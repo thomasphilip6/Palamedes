24f3c969e19150feebf40a55adb519205b8519df
Thomas Philip <thomas.philip@ecam-strasbourg.eu>
2023-01-02 22:14:57 +0100

    Improve chekEveryCell function and implement OOP

--- diffCheck.py
+++ diffCheck.py
@@ -28,71 +28,107 @@ move2=np.array(Image.open('move2.jpg').resize((400,400)))
 coordinates, cells = calibration(board)
 counter=0
 
-def AIcheck(board,image1,image2,cell=""):
-    global counter
-    name=""
-    counter=counter+1
-    ROI1,ROI2=buildROI(board,image1,image2,cell)
-    name1="ROIA"+ str(counter)+".png"
-    name2="ROIB"+ str(counter)+".png"
-    data1=Image.fromarray(ROI1)
-    data2=Image.fromarray(ROI2)
-    data1.save(name1)
-    data2.save(name2)
+class imageDifference:
+    def __init__(self,coordinates,cells,emptyBoard,emptyBoardData,image1,image2):
+        self.coordinates=coordinates
+        self.cells=cells
+        self.emptyBoard=emptyBoard
+        self.emptyBoardData=emptyBoardData
+        self.image1=image1
+        self.image2=image2
 
-    firstImage = model.encode(Image.open(name1))
-    secondImage = model.encode(Image.open(name2))
-    text = model.encode(['ROIA','ROIB'])
+    def AIcheck(self,cell=""):
+        global counter
+        name=""
+        move=False
+        remark=""
+        thresholdDown=85
+        thresholdUp=99
+        counter=counter+1
+        ROI1,ROI2=self.buildROI(cell)
+        name1="ROIA"+ str(counter)+".png"
+        name2="ROIB"+ str(counter)+".png"
+        data1=Image.fromarray(ROI1)
+        data2=Image.fromarray(ROI2)
+        data1.save(name1)
+        data2.save(name2)
 
-    #cos_scores = util.cos_sim(firstImage,text)
-    #print(cos_scores)
-    #print("now trying something else")
-    cos_scores2 = util.cos_sim(firstImage,secondImage)
-    print(cos_scores2)
+        firstImage = model.encode(Image.open(name1))
+        secondImage = model.encode(Image.open(name2))
+        text = model.encode(['ROIA','ROIB'])
+
+        cos_scores = util.cos_sim(firstImage,secondImage)
+        """
+        cos_scores=str(cos_scores[0][0])
+        cos_scores=cos_scores.replace('tensor(','')
+        cos_scores=cos_scores.replace(')','')
+        """
+        print(cos_scores)
+        if (cos_scores*100) < thresholdDown:
+            move=True
+            remark="score under 87 : real Move "
+        elif (cos_scores*100) > thresholdDown and (cos_scores*100) < thresholdUp:
+            move=False
+            remark="score between 87 and 98 : noise"
+        else:
+            move=False
+            remark="score over 99 : same image twice"
+
+        return move,remark
 
 
-def checkRessemblance(ROI1,ROI2):
-    ROI1=cv2.cvtColor(ROI1, cv2.COLOR_BGR2GRAY)
-    ROI2=cv2.cvtColor(ROI2, cv2.COLOR_BGR2GRAY)
-    score, diff = structural_similarity(ROI1, ROI2, full=True)
-    #print("Similarity Score: {:.3f}%".format(score*100))
-    return score*100
+    def checkRessemblance(self,ROI1,ROI2):
+        ROI1=cv2.cvtColor(ROI1, cv2.COLOR_BGR2GRAY)
+        ROI2=cv2.cvtColor(ROI2, cv2.COLOR_BGR2GRAY)
+        score, diff = structural_similarity(ROI1, ROI2, full=True)
+        #print("Similarity Score: {:.3f}%".format(score*100))
+        return score*100
 
-def buildROI(board,previousImage,newImage,cell=""):
-    xLowerLeft, yLowerLeft,xUpperRight,yUpperRight=findCell(board,cell)
-    ROI1=previousImage[yUpperRight:yLowerLeft,xLowerLeft:xUpperRight]
-    ROI2=newImage[yUpperRight:yLowerLeft,xLowerLeft:xUpperRight]
-    #cv2.rectangle(image1,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
-    #cv2.rectangle(image2,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
-    return ROI1,ROI2
+    def buildROI(self,cell=""):
+        xLowerLeft, yLowerLeft,xUpperRight,yUpperRight=findCell(self.emptyBoardData,cell)
+        ROI1=self.image1[yUpperRight:yLowerLeft,xLowerLeft:xUpperRight]
+        ROI2=self.image2[yUpperRight:yLowerLeft,xLowerLeft:xUpperRight]
+        #cv2.rectangle(image1,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
+        #cv2.rectangle(image2,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
+        return ROI1,ROI2
 
-def checkEveryCell(image1,image2):
-    diffArray=[]
-    for cell in cells:
-        ROI1,ROI2=buildROI(boardWitness,image1,image2,cell)
-        score = checkRessemblance(ROI1, ROI2)
+    def checkEveryCell(self):
+        diffArray=[]
+        for cell in self.cells:
+            ROI1,ROI2=self.buildROI(cell)
+            score = self.checkRessemblance(ROI1, ROI2)
 
-        if score < threshold:
-            xLowerLeft, yLowerLeft,xUpperRight,yUpperRight=findCell(boardWitness,cell)
-            cv2.rectangle(image1,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
-            cv2.rectangle(image2,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
+            if score < threshold:
+                xLowerLeft, yLowerLeft,xUpperRight,yUpperRight=findCell(self.emptyBoardData,cell)
+                
+                if score > 40 and score < 55:
+                    move, remark = self.AIcheck(cell)
+                    print(remark)
+                    if move==True:
+                        print("There is a difference at : ")
+                        print(score)
+                        print(cell)
+                        print("")
+                        diffArray.append(cell)
+                        cv2.rectangle(image1,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
+                        cv2.rectangle(image2,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
+                else:
+                    print("There is a difference at : ")
+                    print(score)
+                    print(cell)
+                    print("")
+                    diffArray.append(cell)
+                    cv2.rectangle(image1,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
+                    cv2.rectangle(image2,(xLowerLeft,yLowerLeft),(xUpperRight,yUpperRight),(0,0,255),2)
+        return diffArray
 
-            print("There is a difference at : ")
-            print(score)
-            print(cell)
-            print("")
-            diffArray.append(cell)
-    return diffArray
-
-#checkEveryCell(image0,image1)
-AIcheck(boardWitness,image0,image1,"A1")
-AIcheck(boardWitness,image0,image1,"E2")
-AIcheck(boardWitness,image0,image1,"D4")
+#creating an instance
+move1=imageDifference(coordinates,cells,board,boardWitness,image0,image1)
+move1.checkEveryCell()
 """
-data1=Image.fromarray(image1ROI)
-data2=Image.fromarray(image2ROI)
-data1.save('image1ROI.png')
-data2.save('image2ROI.png')
+move1.AIcheck("A1")
+move1.AIcheck("E2")
+move1.AIcheck("D4")
 """
 print("press x to quit")
 
